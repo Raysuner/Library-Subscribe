c096767c17723f87cc943724f5e80a888906d7a3
Raysuner <1669232805@qq.com>
2019-05-11 16:57:00 +0800

    data path

--- my_dataset.py
+++ my_dataset.py
@@ -7,8 +7,10 @@
 @LastEditors: Raysuner
 @Email: 17775306795@163.com
 @Date: 2019-05-09 19:14:40
-@LastEditTime: 2019-05-09 19:54:37
+@LastEditTime: 2019-05-11 15:46:53
 '''
+
+import datetime
 
 train_dir = './project/Subscribe/dataset/train/'
 
@@ -23,3 +25,10 @@ verify_path = './project/Subscribe/img/verify.png'
 screen_path = './project/Subscribe/img/screen.png'
 
 model_path = './project/Subscribe/model/model.cy'
+
+def get_login_url():
+    local_time =datetime.datetime.now()
+    day_time = str(local_time.year) + '-' + str(local_time.month) + '-' + str(local_time.day + 1)
+    login_url = 'http://219.231.8.121/Home/Web/area?area=9&segment=' + '17319' + '&day=' + day_time + '&startTime=07:00&endTime=22:00'
+    print(login_url)
+    return login_url
